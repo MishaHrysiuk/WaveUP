26f6bc0a435f36a489bcc104fb888f9a8116cfb3
MishaHrysiuk <0973853103h@gmail.com>
2021-03-24 15:50:19 +0200

    Створення хранилищ.

---
+++ WaveUP.Domain/Abstract/IGameRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using WaveUP.Domain.Entities;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WaveUP.Domain.Abstract
+{
+    interface IGameRepository
+    {
+        IEnumerable<Instrument> Instruments { get; }
+    }
+}
---
+++ WaveUP.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Web.Mvc;
+using Moq;
+using Ninject;
+using WaveUP.Domain.Abstract;
+using WaveUP.Domain.Entities;
+
+namespace WaveUP.WebUI.Infrastructure
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private IKernel kernel;
+
+        public NinjectDependencyResolver(IKernel kernelParam)
+        {
+            kernel = kernelParam;
+            AddBindings();
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return kernel.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return kernel.GetAll(serviceType);
+        }
+
+        private void AddBindings()
+        {
+            // Здесь размещаются привязки
+            Mock<IInstrumenteRepository> mock = new Mock<IInstrumentRepository>();
+            mock.Setup(m => m.Instruments).Returns(new List<Instrument>
+            {
+                new Instrument { Name = "Guitar", Price = 1000 },
+                new Instrument { Name = "Violin", Price=1550 },
+                new Instrument { Name = "Piano", Price=10000 }
+            });
+            kernel.Bind<IInstrumentRepository>().ToConstant(mock.Object);
+        }
+    }
+}
