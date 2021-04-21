529b2363e74c14afd5b6413cb11c54404026138b
MishaHrysiuk <0973853103h@gmail.com>
2021-04-21 11:06:11 +0300

    Створення адмін панелі

---
+++ WaveUP.WebUI/Controllers/AdminController.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Mvc;
+using WaveUP.Domain.Abstract;
+
+namespace WaveUP.WebUI.Controllers
+{
+    public class AdminController : Controller
+    {
+        IInstrumentRepository repository;
+
+        public AdminController(IInstrumentRepository repo)
+        {
+            repository = repo;
+        }
+
+        public ViewResult Index()
+        {
+            return View(repository.Instruments);
+        }
+    }
+}
